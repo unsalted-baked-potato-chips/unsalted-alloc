0ead6edb67063be11a3d1adf06e618aafc058de5
Linux User <robert@DESKTOP-QS3F3ME.localdomain>
2022-01-02 06:21:36 +0000

    Added linear allocator

--- include/ualloc.h
+++ include/ualloc.h
@@ -1,7 +1,21 @@
 #ifndef UNSALTED_UALLOC_H
 #define UNSALTED_UALLOC_H
 
+struct _linear_allocator {
+    void * buffer;
+    void * position;
+    size_t size;
+};
+
+typedef _linear_allocator linear_allocator;
+
 int8_t heap_alloc(size_t, void**);
 void   heap_free(void**);
+
+int8_t lin_alloc_init(size_t, linear_allocator *);
+int8_t lin_alloc(size_t, linear_allocator *, void **);
+void lin_free(linear_allocator *);
+void lin_clear(linear_allocator *);
+
 
 #endif //UNSALTED_UALLOC_H
---
+++ src/lin_alloc.c
@@ -0,0 +1,28 @@
+#include "ualloc.h"
+
+int8_t lin_alloc_init(size_t size, linear_allocator * allocator){
+    allocator->buffer = malloc(size);
+    if (!allocator->buffer){
+        *allocator = {NULL, NULL, 0};
+        return 1;
+    }
+    allocator->position = allocator->buffer;
+    allocator->size = size;
+}
+
+int8_t lin_alloc(size_t size, linear_allocator * allocator, void ** buffer){
+    if (allocator->position + size > allocator->buffer+allocator->size){
+        return 1;
+    }
+    allocator->position += size;
+    buffer = allocator->position;
+}
+
+void lin_clear(linear_allocator * allocator){
+    allocator->position = allocator->buffer;
+}
+
+void line_free(linear_allocator * allocator){
+    free(allocator->buffer);
+    *allocator = {NULL,NULL,0};
+}
